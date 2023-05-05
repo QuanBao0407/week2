37455b37d4f205bb44d7e4694949ed5cd783de09
QuanBao0407 <114390731+QuanBao0407@users.noreply.github.com>
2023-05-05 14:20:10 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,7 @@
+package people;
+
+interface Employee {
+    void work();
+    void takeABreak();
+
+}
---
+++ StudentImpl.java
@@ -0,0 +1,16 @@
+package people;
+
+public class StudentImpl extends People implements Student{
+    public StudentImpl(String name, int age, boolean gender) {
+        super(name, age, gender);
+    }
+    public void eat() {
+
+    }
+    public void sleep(){}
+    public void registerCourse(String course) {}
+    public void attendClass() {
+
+    }
+}
+
