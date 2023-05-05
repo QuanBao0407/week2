03314db4878088eb58ded1ff825ace955551c113
QuanBao0407 <114390731+QuanBao0407@users.noreply.github.com>
2023-05-05 15:46:56 +0700

    Add files via upload

---
+++ Movaable.java
@@ -0,0 +1,8 @@
+package movaable;
+
+public interface Movaable {
+    public abstract double moveDown();
+    public abstract double moveLeft();
+    public abstract double moveRight();
+    public abstract double moveUp();
+}
---
+++ MovablePoint.java
@@ -0,0 +1,24 @@
+package movaable;
+
+public class MovablePoint implements Movaable{
+
+    @Override
+    public double moveDown() {
+        return 0;
+    }
+
+    @Override
+    public double moveLeft() {
+        return 0;
+    }
+
+    @Override
+    public double moveRight() {
+        return 0;
+    }
+
+    @Override
+    public double moveUp() {
+        return 0;
+    }
+}
