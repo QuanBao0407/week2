95ec4b25652c08f05723507995daa9e34351828c
QuanBao0407 <114390731+QuanBao0407@users.noreply.github.com>
2023-05-05 16:08:59 +0700

    Add files via upload

---
+++ MovableCircle.java
@@ -0,0 +1,32 @@
+package Mova;
+
+public class MovableCircle extends MovablePoint implements Movable {
+    int radius;
+    MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius, MovablePoint center) {
+        super(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+        this.center = center;
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
